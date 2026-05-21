967bd472ef5223510be7951c288aff49439aabc6
202207056 <kwv03@naver.com>
2026-05-21 15:52:17 +0900

    2026-05-21

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var {getConnection} = require('./connect');
+var oracledb = require('oracledb');
 
 /* 로그인 페이지 */
 router.get('/login', function(req, res, next) {
   res.render('index', {title:'로그인', pageName:'login.ejs'});
+});
+
+router.post('/login', async function(req, res){
+  const scode = req.body.scode;
+  let con;
+  try{
+      con = await getConnection();
+      let sql="select * from students where scode=:scode";
+      let result= await con.execute(sql, {scode}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
+      console.log(result.rows[0]);
+      res.send(result.rows[0]);
+  }catch(err){
+      console.log('로그인체크', err.message);
+  }finally{
+      if(con) await con.close(); 
+  }
 });
 
 module.exports = router;
